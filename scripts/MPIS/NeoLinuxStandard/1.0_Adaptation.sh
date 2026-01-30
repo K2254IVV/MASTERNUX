9e0ff763705783b2279a56ae705249dc31ef3d97
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2026-01-30 19:10:02 +0300

    Update 1.0_Adaptation.sh

--- scripts/MPIS/NeoLinuxStandard/1.0_Adaptation.sh
+++ scripts/MPIS/NeoLinuxStandard/1.0_Adaptation.sh
@@ -6,7 +6,7 @@ echo "[1/5] 🔄 Система Обновляется... "
 pacman -Syu --noconfirm
 
 echo "[2/5] 📦 Установка Пакетов..."
-curl -fsSL https://github.com/K2254IVV/MASTERNUX/raw/refs/heads/main/scripts/MPIS/NeoLinuxStandard/pkglist.txt | sudo pacman -S --noconfirm -
+curl -fsSL https://raw.githubusercontent.com/K2254IVV/MASTERNUX/refs/heads/main/scripts/MPIS/NeoLinuxStandard/pkglist.txt | sudo pacman -S --noconfirm -
 
 echo "[3/5] 📦 Установка yay..."
 mkdir -p /tmp/NeoLinux
@@ -18,10 +18,10 @@ if ! command -v yay &> /dev/null; then
 fi
 
 echo "[4/5] 📦 Установка Пакетов из AUR..."
-curl -fsSL https://github.com/K2254IVV/MASTERNUX/raw/refs/heads/main/scripts/MPIS/NeoLinuxStandard/AURpkglist.txt | yay -S --noconfirm --answerdiff None --answeredit None -
+curl -fsSL https://raw.githubusercontent.com/K2254IVV/MASTERNUX/refs/heads/main/scripts/MPIS/NeoLinuxStandard/AURpkglist.txt | yay -S --noconfirm --answerdiff None --answeredit None -
 
 echo "[5/5] 📦 Установка Пакетов из Flathub..."
-curl -fsSL https://github.com/K2254IVV/MASTERNUX/raw/refs/heads/main/scripts/MPIS/NeoLinuxStandard/FHpkglist.txt | xargs flatpak install -y --noninteractive
+curl -fsSL https://raw.githubusercontent.com/K2254IVV/MASTERNUX/refs/heads/main/scripts/MPIS/NeoLinuxStandard/FHpkglist.txt | xargs flatpak install -y --noninteractive
 
 echo "[6/5] 🎨 Установка Тем и Иконок KDE... [1/3]"
 git clone https://github.com/yeyushengfan258/Win11OS-kde /tmp/NeoLinux/Win11Theme
@@ -46,7 +46,7 @@ cd -
 
 echo "[7/8] 🖥️ Настройка Kitty..."
 mkdir -p ~/.config/kitty
-curl -fsSL "https://github.com/K2254IVV/MASTERNUX/raw/refs/heads/main/scripts/MPIS/NeoLinuxStandard/config.txt" -o ~/.config/kitty/kitty.conf
+curl -fsSL "https://raw.githubusercontent.com/K2254IVV/MASTERNUX/refs/heads/main/scripts/MPIS/NeoLinuxStandard/config.txt" -o ~/.config/kitty/kitty.conf
 
 echo "[8/8] 🔓 Установка Zapret..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Snowy-Fluffy/zapret.installer/refs/heads/main/installer.sh)"
